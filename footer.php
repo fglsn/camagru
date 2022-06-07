2e4c7e8676eb67d9a6950a05d47bba411f87f750
Ilona Shakurova <leverseau19@gmail.com>
2022-06-07 15:05:55 +0300

    Add header, footer, sign-in and sign-up static pages, add favicon and styles

---
+++ footer.php
@@ -0,0 +1,7 @@
+	<footer class="footer small text-center mt-auto text-muted" style="width: 100%;">
+			<div class="container-fluid">
+				<p>Camagru project by Ilona Shakurova. Hive Helsinki 2022</p>
+			</div>
+	</footer>
+	</body>
+</html>
---
+++ login.php
@@ -0,0 +1,39 @@
+<?php
+		include("./header.php");
+?>
+
+<main>
+	<div class="form-wrapper">
+		<div class="form-container">
+			<h3 class="form-header" style="font-family: 'Cookie', cursive;">CamAgru</h3>
+			<form action="login" method="post" class="form-box">
+				<div class="mb-3">
+					<input type="email" class="form-control" id="email" name="email" placeholder="Email" autocomplete="off" required>
+				</div>
+				<div class="mb-3">
+					<input type="password" class="form-control" id="password" name="password" placeholder="Password" autocomplete="off" type="password" required>
+				</div>
+				<div class="d-grid gap-2">
+					<button class="btn btn-primary" type="submit">Log in</button>
+				</div>
+				<div class="separator"><div class="line"></div><div class="or">OR</div><div class="line"></div></div>
+				<div class="form-footer"><a href="/accounts/password/reset/" tabindex="0">Forgot password?</a></div>
+			</form>
+		</div>
+		<div class="form-container">
+				<div class="sign-text">
+					<p class="sign-up">Don't have an account?
+						<a data-testid="sign-up-link" href="./signup.php" tabindex="0">
+							<span class="sign-link">
+								Sign up
+							</span>
+						</a>
+					</p>
+				</div>
+			</div>
+	</div>
+</main>
+
+<?php
+		include("./footer.php");
+?>
