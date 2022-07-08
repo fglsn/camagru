2e2b3426382d9b819ab8d332f60edf1373270d74
Ilona Shakurova <ishakuro@student.hive.fi>
2022-07-08 15:50:14 +0300

    Add webcam preview and buttos to take a pic

--- templates/upload.php
+++ templates/upload.php
@@ -1,9 +1,10 @@
 <?php
 	include(__DIR__ . "/header.php");
 ?>
-<!-- <video id="" width="340" height="240" autoplay></video> -->
+
 <!-- https://stackoverflow.com/questions/572768/styling-an-input-type-file-button -->
 <!-- https://stackoverflow.com/questions/2189615/how-to-get-file-name-when-user-select-a-file-via-input-type-file -->
+
 <main class="container-fluid">
 	<?php if ($info !== '') {
 			echo '<span class="info-log" id="snackbar" style="display:block">' . $info . '</span>'; }?>
@@ -19,7 +20,21 @@
 					<button class="btn btn-primary" id="upload-btn" type="submit" name="upload">Upload</button>
 				</div>
 			</form>
-			<h4 class="form-header-light">Or</h4>
+			<h4 class="form-header-light">Or share your picture using Webcamera!</h4>
+
+			<form class="container camera" style="max-width: 80%">
+				<div class="webcam-container container">
+					<canvas id="canvas" class="container">
+						<video autoplay="true" class="container" id="webcam" onclick=stop(e)>
+						</video>
+					</canvas>
+				</div>
+				<div class="cam-buttons">
+					<button class="btn btn-success webcam-btn">Take a pic</button>
+					<button class="btn btn-danger webcam-btn" onclick="stop(e)">Stop the stream</button>
+				</div>
+			</form>
+
 		</div>
 	</div>
 </main>
@@ -40,6 +55,56 @@
 		}
 	}
 	myFunction();
+</script>
+
+<!-- <script type="text/javascript">
+	var video = document.querySelector("#webcam");
+
+	if (navigator.mediaDevices.getUserMedia) {
+		navigator.mediaDevices
+		.getUserMedia({ video: true })
+		.then(function (stream) {
+		video.srcObject = stream;
+		})
+		.catch(function (err0r) {
+		console.log("Something went wrong!");
+		});
+	// https://www.kirupa.com/html5/accessing_your_webcam_in_html5.htm
+} -->
+
+</script>
+
+<script type="text/javascript">
+	var canvas = document.getElementById('canvas');
+	var ctx = canvas.getContext('2d');
+	var video = document.getElementById('webcam');
+
+	// set canvas size = video size when known
+	video.addEventListener('loadedmetadata', function() {
+	canvas.width = video.videoWidth;
+	canvas.height = video.videoHeight;
+	});
+
+	if (navigator.mediaDevices.getUserMedia) {
+		navigator.mediaDevices
+		.getUserMedia({ video: true })
+		.then(function (stream) {
+		video.srcObject = stream;
+		})
+		.catch(function (err0r) {
+		console.log("Something went wrong!");
+		});
+	}
+
+	video.addEventListener('play', function() {
+		var $this = this; //cache
+		(function loop() {
+			if (!$this.paused && !$this.ended) {
+				ctx.drawImage($this, 0, 0);
+				setTimeout(loop, 1000 / 80); // drawing at 30fps
+			}
+		})();
+	}, 0);
 </script>
 
 <?php
