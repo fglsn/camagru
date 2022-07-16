adf392670cae7ea79c215019cac6340b284a5daa
Ilona Shakurova <leverseau19@gmail.com>
2022-07-16 14:44:46 +0300

    Add likes table and references on delete to all tables

--- config/init_v1.sql
+++ config/init_v1.sql
@@ -3,17 +3,17 @@ create database if not exists camagru_db;
 
 -- Users table --
 create table if not exists camagru_db.users (
-		user_id int auto_increment primary key,
-		username varchar(10) not null,
-		email varchar(100) not null,
-		password text not null,
-		active tinyint not null default 0,
-		activation_code text not null,
-		activated_at datetime default null,
-		notifications tinyint not null default 1,
-		created_at timestamp not null default current_timestamp(),
-		updated_at datetime default current_timestamp() on update current_timestamp()
-	);
+	user_id int auto_increment primary key,
+	username varchar(10) not null,
+	email varchar(100) not null,
+	password text not null,
+	active tinyint not null default 0,
+	activation_code text not null,
+	activated_at datetime default null,
+	notifications tinyint not null default 1,
+	created_at timestamp not null default current_timestamp(),
+	updated_at datetime default current_timestamp() on update current_timestamp()
+);
 
 -- Create unique indexes to store only unique username and email, handle pdo-exeption errors in signup.php --
 create unique index username_index on camagru_db.users (username);
@@ -23,10 +23,13 @@ create unique index activation_code_index on camagru_db.users (activation_code);
 
 -- Password reset request table --
 create table if not exists camagru_db.password_reset_request (
-		id int(10) unsigned not null auto_increment primary key,
-		user_id int(10) unsigned not null,
-		requested_at datetime not null,
-		token varchar(255) collate utf8_unicode_ci not null
+	id int not null auto_increment primary key,
+	user_id int not null,
+	requested_at datetime not null,
+	token varchar(255) collate utf8_unicode_ci not null,
+	foreign key (user_id)
+		references camagru_db.users (user_id)
+		on delete cascade
 );
 
 -- Create unique index to store only unique tokens, handle pdo-exeption errors in reset_pwp.php --
@@ -41,17 +44,42 @@ create table if not exists camagru_db.posts (
 	picture_path text not null,
 	picture_description varchar(250) not null,
 	created_at timestamp not null default current_timestamp(),
-	webcam tinyint not null default 0
+	webcam tinyint not null default 0,
+	foreign key (owner_id)
+		references camagru_db.users (user_id)
+		on delete cascade
 );
 
 -- Comment table --
 create table if not exists camagru_db.comments (
-	comment_id int(11) unsigned auto_increment primary key,
+	comment_id int auto_increment primary key,
 	post_id int not null,
 	post_owner varchar(10) not null,
 	comment varchar(250) not null,
 	commentator varchar(10) not null,
-	created_at timestamp not null default current_timestamp()
+	created_at timestamp not null default current_timestamp(),
+	foreign key (post_id)
+		references camagru_db.posts (post_id)
+		on delete cascade,
+	foreign key (post_owner)
+		references camagru_db.users (username)
+		on delete cascade,
+	foreign key (commentator)
+		references camagru_db.users (username)
+		on delete cascade
+);
+
+-- Likes table --
+create table if not exists camagru_db.likes (
+	like_id	int auto_increment primary key,
+	post_id int not null,
+	user_id int not null,
+	foreign key (post_id)
+		references camagru_db.posts (post_id)
+		on delete cascade,
+	foreign key (user_id)
+		references camagru_db.users (user_id)
+		on delete cascade 
 );
 
 -- Create migrations to check if db is expected state --
--- src/validations.php
+++ src/validations.php
@@ -43,9 +43,9 @@
 	}
 
 	function validate_comment($comment) {
-		$comment = input_data($comment);
-		if (!preg_match("/^[a-zA-Z0-9_\-!?. ,@#:)(;'\"]*$/", $comment))
+		if (!preg_match("/^[a-zA-Z0-9_\-+!?. ,@#:)(;'\"]*$/", $comment))
 			throw new ValidationException("Forbidden characters used. Please try again");
+		$comment = input_data($comment);
 		if (strlen($comment) > 250)
 			throw new ValidationException("Invalid length (max 250 chars).");
 		return $comment;
